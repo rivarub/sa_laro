e933709f87b7c68cb97c52bf710eec50a9416a8f
rivarub <rivarubg@gmail.com>
2017-12-03 16:14:53 +0200

    MobileTable finished!

--- js/js.js
+++ js/js.js
@@ -44,11 +44,19 @@ $(document).ready(function () {
     });
 
     var blocks = 1;
-
+//  var tabElems = $("table", $('div.mobileTab'));
+//        var i = tabElems.length;
+//        while (i < 9) {}
+//        for (; i < 9; i++) {}
     $('#add').click(function (event) {
         // jast experiment
 //        $('table.mobileTab', 'div.mobileTab').css("color", "green").add("input[id='add']")
 //                .css("font-size", ".75em");
+
+        //var i = 0;
+        //do {
+//        var tabElems = $("table", $('div.mobileTab'));
+//        for (var i = 0; i < 2; i++) {
         // 1 var clone
 //        var newElems = $('table.mobileTab').clone();
 //        $(newElems).insertAfter('table.mobileTab');
@@ -63,32 +71,33 @@ $(document).ready(function () {
 
         // 3 var
         blocks++;
+        if(blocks <=8) {
         var in1Elem = $('<td>')
-                .append('<input type="text" name="medical_problem_'+blocks+'" id="medical_problem_2"/>');
+                .append('<input type="text" name="medical_problem_' + blocks + '" id="medical_problem_' + blocks + '"/>');
         var tr1Elems = $("<tr/>")
                 .append('<td>Medical Problem</td>')
                 .append(in1Elem);
 
         var in2Elem = $('<td>')
-                .append('<input type="text" name="medication_2" id="medication_2"/>');
+                .append('<input type="text" name="medication_' + blocks + '" id="medication_' + blocks + '"/>');
         var tr2Elems = $("<tr/>")
                 .append('<td>Medication (name/dose)</td>')
                 .append(in2Elem);
 
         var in3Elem = $('<td>')
-                .append('<input type="text" name="start_date_2" id="start_date_2" placeholder="mm/dd/yyyy"/>');
+                .append('<input type="text" name="start_date_' + blocks + '" id="start_date_' + blocks + '" placeholder="mm/dd/yyyy"/>');
         var tr3Elems = $("<tr/>")
                 .append('<td>Start Date</td>')
                 .append(in3Elem);
 
         var in4Elem = $('<td>')
-                .append('<input type="text" name="stop_date_2" id="stop_date_2" placeholder="mm/dd/yyyy"/>');
+                .append('<input type="text" name="stop_date_' + blocks + '" id="stop_date_' + blocks + '" placeholder="mm/dd/yyyy"/>');
         var tr4Elems = $("<tr/>")
                 .append('<td>Stop Date</td>')
                 .append(in4Elem);
 
         var in5Elem = $('<td>')
-                .append('<input type="text" name="currently_taking_2" id="currently_taking_2"/>');
+                .append('<input type="text" name="currently_taking_' + blocks + '" id="currently_taking_' + blocks + '"/>');
         var tr5Elems = $("<tr/>")
                 .append('<td>Currently Taking?</td>')
                 .append(in5Elem);
@@ -99,10 +108,13 @@ $(document).ready(function () {
                 .append(tr3Elems)
                 .append(tr4Elems)
                 .append(tr5Elems);
-        $('div.mobileTab table.mobileTab').after(newElems);
-
-        // end of 3 var
-        //вибір елементів
+        $('div.mobileTab input#add').before(newElems);
+    } else {
+//        } //while (i <= 8);
+        $(this).unbind("click");
+    }
+                // end of 3 var
+                //вибір елементів
 //        $('input[name="medical_problem_]').parentsUntil('div.mobileTab').find('input[name^="medical_problem_]')
 //                .prev('input[name^="medical_problem_]')
 //                ;               
@@ -113,10 +125,10 @@ $(document).ready(function () {
 //                return  "carnation.png";
 //            }
 //        });
-        //        
-        newElems.each(function (index, elem) {
-            console.log("New element: " + elem.tagName + " " + elem.className);
-        });
+                //        
+//                newElems.each(function (index, elem) {
+//                    console.log("New element: " + elem.tagName + " " + elem.className);
+//                });
 
 //            newElems.children('img').each(function(index, elem) {
 //                console.log("Child: " + elem.tagName + " " + elem.src);
